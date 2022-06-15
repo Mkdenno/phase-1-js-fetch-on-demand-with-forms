28f31b94a3343a1fc06009115fe15d83be9a7962
Mkdenno <dennis.kiprop@student.moringaschool.com>
2022-06-15 07:48:47 +0300

    commit tree ca18e672f0c3dd95983c0caad28f2b52fe3f6c29

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const init = () => {
+  const inputForm=  document.querySelector('form').addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const inputEl=document.getElementById('searchByID');
+        console.log(inputEl.value)
+
+        fetch(`http://localhost:3000/movies/${inputEl.value}`)
+        .then(res=>res.json())
+        .then(data=>{
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+             title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    })
   
 }
 
